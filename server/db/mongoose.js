ce744a663e88e0ecfc7923a0060afbcd7d610dab
Robert P. Churchill <products@rpchurchill.com>
2018-11-15 16:56:08 -0500

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+//mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {useNewUrlParser: true});
 
 //mongoose.set('useCreateIndex', true);
 

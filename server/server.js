7f1b38e8e0e0a68fce4010380e6717f3d0c8295e
Robert P. Churchill <products@rpchurchill.com>
2018-11-16 16:49:22 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,29 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET todos/1234324
+app.get('/todos/:id', (req, res) => {
+  var todoId = req.params.id;
+  
+  if (!ObjectID.isValid(todoId)) {
+    console.log('todo ID not valid');
+    return res.status(404).send();
+  }
+
+  Todo.findById(todoId).then((todo) => {
+    if (!todo) {
+      console.log('todo Id not found');
+      return res.status(400).send();
+    }
+    console.log('Todo By Id', todo);
+    res.send(todo);
+  }); //.catch((e) => {
+  //  console.log('Does this ever happen?',e);
+  //  res.status(400).send();
+  //});
+  
 });
 
 app.listen(3000, () => {

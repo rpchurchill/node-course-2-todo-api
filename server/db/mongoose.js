21649141774359ba9755da5a223a25928294bd00
Robert P. Churchill <products@rpchurchill.com>
2018-11-19 17:16:27 -0500

    Set up app for Heroku with mLab MongoDB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp', {useNewUrlParser: true});
 
 //mongoose.set('useCreateIndex', true);
 

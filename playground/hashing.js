4d08319b2a44d8587141578151c3bd46e268f5b1
Robert P. Churchill <products@rpchurchill.com>
2018-12-01 19:37:32 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc'); 
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded); 
+var hashedPassword = '$2a$10$/2QkCAwfjZLSn0K2L/muAegzlLKE3GlXCBNbiX6PcuxwZyBi6w9va';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+/////---------- second demos ----------------------
+//var data = {
+//  id: 10
+//};
+
+//var token = jwt.sign(data, '123abc'); 
+//console.log(token);
+
+//var decoded = jwt.verify(token, '123abc');
+//console.log('decoded', decoded); 
 
 /////---------- initial demos ---------------------
 // var message = 'I am user number 3';

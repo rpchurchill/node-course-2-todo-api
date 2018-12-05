b4de34e02c082aa49b8b50980b46cff53ffd5013
Robert P. Churchill <products@rpchurchill.com>
2018-12-05 15:58:13 -0500

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -123,7 +123,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// POST /users/login {email, password-hashed}
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email','password']);
   
